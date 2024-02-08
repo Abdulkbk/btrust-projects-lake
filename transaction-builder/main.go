0b2db6fd0315f5d4b8f15797b9af64287656a75f
Node <abdoollahikbk@gmail.com>
2024-02-08 16:13:05 +0100

    mod: reevaluating code

--- transaction-builder/main.go
+++ transaction-builder/main.go
@@ -17,16 +17,15 @@ func main() {
 
 	fmt.Println(redeemScript)
 
-	// ==================== 2
 	p2shAddress := generateAddressFromRedeemScript(redeemScript)
 
 	fmt.Println(p2shAddress)
 
-	// ==================== 3
-	prevTxHashStr := ""
-	p2shAddr := ""
-	ammount := btcutil.Amount(100000)
-	sendTrx := transactionContructor(prevTxHashStr, p2shAddr, ammount)
+	prevTxHashStr := "5dc27ef4b998e1b2bcb62bf53d99a83f703ff110977fc63f72194d65e280a7cb"
+	p2shAddr := "2NEWkaQQEMGyMyFyFXdgmpo1GHN6qiPhP5t"
+	amount := btcutil.Amount(100000)
+	sendTrx := transactionContructor(prevTxHashStr, p2shAddr, amount)
+
 	spendTrx := TransactionSpenderConstructor(sendTrx.TxHash().String(), btcutil.Amount(100000), "2NEWkaQQEMGyMyFyFXdgmpo1GHN6qiPhP5t", "2NEWkaQQEMGyMyFyFXdgmpo1GHN6qiPhP5t")
 
 	fmt.Println(spendTrx)
@@ -34,37 +33,48 @@ func main() {
 }
 
 func generateRedeemScript() string {
-	preImage := "Btrust Builders"
+	// preImage := "Btrust Builders"
+
+	// Byte encoding of "Btrust BUlders"
+	preImage := "427472757374204275696c64657273"
 
 	hash := sha256.Sum256([]byte(preImage))
 
-	redeemScript := hex.EncodeToString(hash[:])
+	lockHex := hex.EncodeToString(hash[:])
+
+	// The redeem script for the p2sh
+	redeemScript := fmt.Sprintf("OP_SHA256 %s OP_EQUAL", lockHex)
+
 	return redeemScript
 }
 
 func generateAddressFromRedeemScript(redeemScript string) btcutil.Address {
-	redeemScriptBytes, err := hex.DecodeString(redeemScript)
+	// Convert the redeem script to bytes
+	redeemScriptBytes := []byte(redeemScript)
 
-	if err != nil {
-		fmt.Println("Error while decoding redeem script", err)
-	}
+	// Hash the redeem script with Sha256 to give a 32 byte hash
+	hash := sha256.Sum256(redeemScriptBytes)
 
-	hash2 := sha256.Sum256(redeemScriptBytes)
+	// Hash the result with RIPEMD160  to give a 20 byte hash
+	ripemd160Hash := btcutil.Hash160(hash[:])
 
-	ripemd160Hash := btcutil.Hash160(hash2[:])
-
+	// Append the 0x05 version prefix
 	p2shHash := append([]byte{0x05}, ripemd160Hash...)
 
+	// Double hash the result with Sha256 to give a 32 byte hash
 	checksum := sha256.Sum256(p2shHash)
 	checksum = sha256.Sum256(checksum[:])
 
+	// Append the checksum for the p2sh address
 	p2shHash = append(p2shHash, checksum[:4]...)
 
+	// Convert the p2sh hash to a p2sh address
 	p2shAddress, _ := btcutil.NewAddressScriptHash(p2shHash, &chaincfg.SigNetParams)
 	return p2shAddress
 }
 
 func transactionContructor(prevTxHashStr, p2ScriptAddrStr string, amount btcutil.Amount) *wire.MsgTx {
+
 	tx := wire.NewMsgTx(wire.TxVersion)
 
 	prevTxHash, err := chainhash.NewHashFromStr(prevTxHashStr)
@@ -74,19 +84,21 @@ func transactionContructor(prevTxHashStr, p2ScriptAddrStr string, amount btcutil
 		return tx
 	}
 
+	// Creating the input for the transaction (referencing a utxo)
 	prevTxOutPoint := wire.NewOutPoint(prevTxHash, 0)
 	txIn := wire.NewTxIn(prevTxOutPoint, nil, nil)
 	tx.AddTxIn(txIn)
 
 	// p2ScriptAddrStr := "2NEWkaQQEMGyMyFyFXdgmpo1GHN6qiPhP5t"
-	p2sAddress, err := btcutil.DecodeAddress(p2ScriptAddrStr, &chaincfg.SigNetParams)
+	p2shAddress, err := btcutil.DecodeAddress(p2ScriptAddrStr, &chaincfg.SigNetParams)
 
 	if err != nil {
-		fmt.Println("Error while decoding p2s address", err)
+		fmt.Println("Error while decoding p2sh address", err)
 		return tx
 	}
 
-	script, err := txscript.PayToAddrScript(p2sAddress)
+	//
+	script, err := txscript.PayToAddrScript(p2shAddress)
 
 	if err != nil {
 		fmt.Println("Error while creating p2s script", err)
@@ -132,7 +144,7 @@ func TransactionSpenderConstructor(prevTxHashStr string, mainAmount btcutil.Amou
 	mainTxOut := wire.NewTxOut(int64(mainAmount), mainScript)
 	tx.AddTxOut(mainTxOut)
 
-	inputAmount := int64(100000000)                 // Total amount from previous transaction
+	inputAmount := int64(10000)
 	changeAmount := inputAmount - int64(mainAmount) // Calculate change amount
 	changeAddress, err := btcutil.DecodeAddress(changeAddressStr, &chaincfg.SigNetParams)
 
@@ -160,6 +172,6 @@ func TransactionSpenderConstructor(prevTxHashStr string, mainAmount btcutil.Amou
 
 	tx.TxIn[0].SignatureScript = unlockingScript
 
-	fmt.Println(tx)
+	// fmt.Println(tx)
 	return tx
 }
